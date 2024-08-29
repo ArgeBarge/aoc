c01856337ccfb0816c74d3f407673bf0f22cc804
James Barge <jamesbarge15@gmail.com>
2024-08-29 21:19:11 +0100

    feat: add day 5 part 2

---
+++ 2022/day5/day5_2.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <cctype>
+#include "parser.h"
+
+#define STACK_NUM 9
+
+std::vector<char>* get_initial_state(std::vector<std::string>*);
+void get_final_state(std::vector<std::string>*, std::vector<char>*);
+void move_crates(const int*, std::vector<char>*);
+
+int main(int argc, char **argv)
+{
+    std::vector<std::string> lineList = parse_lines("day5.txt");
+    std::vector<char>* initialState = get_initial_state(&lineList);
+
+    for(int i = 0; i < STACK_NUM; i++)
+    {
+        for(auto it = initialState[i].begin(); it != initialState[i].end(); it++)
+        {
+            std::cout << *it << " ";
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "\n";
+
+    get_final_state(&lineList, initialState);
+    
+    for(int i = 0; i < STACK_NUM; i++)
+    {
+        for(auto it = initialState[i].begin(); it != initialState[i].end(); it++)
+        {
+            std::cout << *it << " ";
+        }
+        std::cout << "\n";
+    }
+
+    delete[] initialState;
+    return 0;
+}
+
+std::vector<char>* get_initial_state(std::vector<std::string>* lineList)
+{
+    //deallocate
+    std::vector<char> *initialState = new std::vector<char>[STACK_NUM];
+    
+    for(auto it = lineList->begin(); it != lineList->end(); it++)
+    {
+        for(int i = 0; i < (*it).length(); i++)
+        {
+            if(std::isupper((*it)[i])) 
+                initialState[(i - 1) / 4].insert(initialState[(i - 1) / 4].begin(), (*it)[i]);
+            else if(std::islower((*it)[i]))
+            {
+                lineList->erase(lineList->begin(), it);
+                return initialState;
+            }
+        }     
+    }
+
+    return NULL;
+}
+
+void get_final_state(std::vector<std::string>* lineList, std::vector<char>* currentState)
+{
+    int currentCommand[3];
+    int number_buffer[2] = {-1, -1};
+    int i = 0;
+
+    for(auto l : *lineList)
+    {
+        
+        i = 0;
+        for(auto it = l.begin(); it != l.end(); it++)
+        {
+            
+            if(std::isdigit(*it))
+            {
+                number_buffer[0] == -1 ? number_buffer[0] = *it - '0' : number_buffer[1] = *it - '0';
+
+                if(std::isdigit(*(it+1)))
+                    continue;
+                else 
+                {
+                    if(number_buffer[1] == -1)
+                        currentCommand[i % 3] = number_buffer[0];
+                    else 
+                        currentCommand[i % 3] = number_buffer[0] * 10 + number_buffer[1];
+
+                    i++;
+
+                    if(i % 3 == 0 && i != 0)
+                    {
+                        std::cout << "current command: " << currentCommand[0] << " " << currentCommand[1] << " " << currentCommand[2] << std::endl;
+                        move_crates(currentCommand, currentState);
+                    }
+                
+                    number_buffer[0] = -1;
+                    number_buffer[1] = -1;
+                }
+            }        
+        }
+    }
+}
+
+void move_crates(const int* currentCommand, std::vector<char>* currentState)
+{
+    int quant = currentCommand[0];
+    int from = currentCommand[1]-1;
+    int to = currentCommand[2]-1;
+
+    std::vector<char> cratesToMove;
+
+    for(int i = 0; i < quant; i++)
+    {
+        cratesToMove.insert(cratesToMove.begin(), currentState[from].back());
+        currentState[from].pop_back();
+    }
+
+    currentState[to].insert(currentState[to].end(), cratesToMove.begin(), cratesToMove.end());
+}   
